7fb872ddd7ff7db8f0da8cf4fabb99982f1b4ac0
Ben Johnson <bwj.johnson@gmail.com>
2014-08-05 22:04:38 -0700

    Added inline select edit for the status and assigned user fields for the visitor list page

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -38,10 +38,14 @@ class VisitorsController < ApplicationController
   end
 
   def update
-    if @visitor.update(visitor_params)
-      redirect_to @visitor, notice: 'Visitor was successfully updated.'
-    else
-      render :edit
+    respond_to do |format|
+      if @visitor.update(visitor_params)
+        format.html { redirect_to @visitor, notice: 'Visitor was successfully updated.' }
+        format.json { render :show, status: :ok, location: @visitor }
+      else
+        format.html { render :edit }
+        format.json { render json: @visitor.errors, status: :unprocessable_entity }
+      end
     end
   end
 
---
+++ app/views/visitors/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @visitor, :id, :name, :email, :phone, :status, :user, :created_at, :updated_at
